290a9a64f1437041b2b50eba396493fb15788906
shkim787 <shkim061198@gmail.com>
2020-04-19 18:01:42 +0900

    [INSERT] start project!!! , add Customer folder

---
+++ 개인공부/React_with_NodeJS/my_app/src/components/Customers.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Customer extends React.Component{
+    render() {
+        return (
+            <div>
+                <h2>김성현</h2>
+                <p>981106</p>
+                <p>남자</p>
+                <p>대학생</p>
+            </div>
+        )
+    }
+}
+
+export default Customer;
