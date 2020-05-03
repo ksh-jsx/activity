0c661c57245d9152caf9a4fdc4ca952365518807
shkim787 <shkim061198@gmail.com>
2020-05-03 15:53:17 +0900

    [INSERT] file 업로드 요청 처리 및 db에 데이터 insert

--- 개인공부/React_with_NodeJS/my_app/server.js
+++ 개인공부/React_with_NodeJS/my_app/server.js
@@ -4,13 +4,18 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const multer = require('multer');
+const upload = multer({dest:'./upload'})
+
 /*db info*/
 const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
 const mysql = require('mysql');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
 
 const connection = mysql.createConnection({
     host: conf.host,
@@ -32,6 +37,22 @@ app.get('/api/customers', (req, res) => {
     )
 });
     
+app.use('/image',express.static('./upload'));
 
+app.post('/api/customers',upload.single('image'),(req,res) =>{
+    let sql = 'INSERT INTO CUSTOMER VALUES (null, ?, ?, ?, ?, ?)';
+    let image = '/image/' + req.file.filename;
+    let name = req.body.name;
+    let birthday = req.body.birthday;
+    let gender = req.body.gender;
+    let job = req.body.job;
+    let params = [image, name, birthday, gender, job];
+    console.log('something inserted!!')
+    connection.query(sql, params,
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    )
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
