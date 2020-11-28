45e815174eaf4ac0263a78ba8c0f58a475b6881a
shkim787 <shkim061198@gmail.com>
2020-11-28 21:08:12 +0900

    기본 세팅 완료

---
+++ 개인공부/FireBase/src/firebase.js
@@ -0,0 +1,13 @@
+  import  firebase from "firebase/app";
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyBJ6LPez_gz2xOyULQNAEw7aauTGJLn5Ug",
+    authDomain: "nwitter-cc029.firebaseapp.com",
+    databaseURL: "https://nwitter-cc029.firebaseio.com",
+    projectId: "nwitter-cc029",
+    storageBucket: "nwitter-cc029.appspot.com",
+    messagingSenderId: "600279533251",
+    appId: "1:600279533251:web:e1907d313039df817b8476"
+  };
+
+  export default firebase.initializeApp(firebaseConfig);
