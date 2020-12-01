f9d7c6511ade32f86b646d7723595a65b93a0dd6
shkim787 <shkim061198@gmail.com>
2020-12-01 21:26:27 +0900

    로그인&가입 기능 구현

--- 개인공부/FireBase/src/components/Router.js
+++ 개인공부/FireBase/src/components/Router.js
@@ -1,28 +1,39 @@
-import React from "react";
-import {HashRouter as Router, Route, Switch} from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import React, { useState } from "react";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
-
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+const AppRouter = ({ isLoggedIn }) => {
+  
   return (
     <Router>
+      {isLoggedIn && <Navigation/>}
       <Switch>
         {isLoggedIn ? (
           <>
             <Route exact path="/">
               <Home/>
             </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
           </>
-         ) : (
-           <Route exact path="/">
-            {" "}
-            <Auth/>
-
+        ) : (
+          <>
+            <Route exact path="/">
+              <Auth/>
+            </Route>
+          </>
+        )};
       </Switch>
     </Router>
-  )
-};
-
+  );
+}
 export default AppRouter;
