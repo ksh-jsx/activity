807a6fa9a67f9cc985b1de144bb4e46365352260
shkim787 <shkim061198@gmail.com>
2020-12-02 20:55:16 +0900

    실시간 글쓰기 crud

--- 개인공부/FireBase/src/components/App.js
+++ 개인공부/FireBase/src/components/App.js
@@ -5,10 +5,13 @@ import { authService } from "fbase";
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setuserObj] = useState(null);
+
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if(user){
         setIsLoggedIn(true);
+        setuserObj(user);
       } else {
         setIsLoggedIn(false);
       }
@@ -17,7 +20,7 @@ function App() {
   }, []);
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn}/> : "Loading..."}
+      {init ?  (<AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />) : ("Loading...")}
       <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
     </>
   );
--- 개인공부/FireBase/src/routes/Home.js
+++ 개인공부/FireBase/src/routes/Home.js
@@ -1,4 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { dbService } from "fbase";
+import Nweet from "components/Nweet";
 
-const Home =() => <span>Home</span>;
+const Home = ({userObj}) => {
+  const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection("nweets").get();
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getNweets();
+    dbService.collection("nweets").onSnapshot(snapshot =>{
+        const nweetArr = snapshot.docs.map((doc) => ({
+            id:doc.id,
+            ...doc.data(),
+        }));
+        setNweets(nweetArr)
+    })
+  }, []);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection("nweets").add({
+      text: nweet,
+      createdAt: Date.now(),
+      creatorId : userObj.uid,
+    });
+    setNweet("");
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={nweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxLength={120}
+        />
+        <input type="submit" value="Nweet" />
+      </form>
+      <div>
+        {
+            nweets.sort((a, b) => a['createdAt'] > b['createdAt'] ? 1 : ((a['createdAt'] < b['createdAt'] ? -1 : 0))).
+            map((nweet) => (
+                <Nweet
+                    key={nweet.id}
+                    nweetObj={nweet}
+                    isOwner={nweet.creatorId === userObj.uid}
+                />
+            ))}
+      </div>
+    </div>
+  );
+};
 export default Home;
