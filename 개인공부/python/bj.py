dad17dbe4433f5852726008aae5fe8d7c00c79ef
shkim787 <shkim061198@gmail.com>
2021-03-18 20:28:58 +0900

    브루트포스 2

--- 개인공부/python/bj.py
+++ 개인공부/python/bj.py
@@ -1,22 +1,7 @@
-lotto = [0] * 7
-
-def go(idx, start, n, m):
-    if idx == 7:
-        tmp = 0
-        for i in lotto:
-            tmp+=i
-        if tmp == 100:
-            lotto.sort()
-            for i in lotto:
-                print(i)
-        return
-
-    for i in range(start, len(n)):
-        lotto[idx] = n[i]
-        go(idx + 1, i + 1, n, m)
-    
-arr = []
-for _ in range(9):
-    arr.append(int(input()))
-
-go(0, 0, arr, 9)
+a = list(map(int,input().split()))
+num = 1
+while 1:
+    if (num-a[0])%15==0 and (num-a[1])%28==0 and (num-a[2])%19==0:
+        print(num)
+        break
+    num+=1
