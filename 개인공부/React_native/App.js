df0ea6534f2b585395d7ac141b7d5f36467ae543
shkim787 <shkim061198@gmail.com>
2021-07-09 21:35:02 +0900

    날씨앱 완성

--- 개인공부/React_native/App.js
+++ 개인공부/React_native/App.js
@@ -1,38 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 import axios from "axios";
+import Weather from "./Weather";
 
 const API_KEY = "c9159c297c3a07125437ffc945d501ef";
 
-export default class extends React.Component {
-  state = {
-    isLoading: true
-  };
-  getWeather = async (latitude, longitude) => {
-    const { data } = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [condition, setCondition] = useState();
+  const [temp, setTemp] = useState();
+
+  const getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
     );
-    console.log(data);
+    setCondition(weather[0].main);
+    setTemp(temp);
+    setIsLoading(false);
   };
-  getLocation = async () => {
+
+  const getLocation = async () => {
     try {
       await Location.requestForegroundPermissionsAsync();
       const {
         coords: { latitude, longitude }
       } = await Location.getCurrentPositionAsync();
-      this.getWeather(latitude, longitude);
-      this.setState({ isLoading: false });
+      getWeather(latitude, longitude);
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
     }
   };
-  componentDidMount() {
-    this.getLocation();
-  }
-  render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
-  }
+
+  useEffect(() => {
+    getLocation();
+  }, [condition,temp]);
+
+  return (
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+          <Weather temp={Math.round(temp)} condition={condition} />
+      )}
+    </>
+  );
 }
+
+export default App;
