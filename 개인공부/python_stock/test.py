7cbedc455db711288950c166e9cdca2e2fca40d7
shkim787 <shkim061198@gmail.com>
2022-03-07 15:47:50 +0900

    자동매매, 자동 로그인

--- 개인공부/python_stock/test.py
+++ 개인공부/python_stock/test.py
@@ -1,6 +1,8 @@
 import win32com.client
- 
- 
+import requests
+import os
+import time
+
 # 연결 여부 체크
 objCpCybos = win32com.client.Dispatch("CpUtil.CpCybos")
 bConnect = objCpCybos.IsConnect
@@ -8,24 +10,63 @@ if (bConnect == 0):
     print("PLUS가 정상적으로 연결되지 않음. ")
     exit()
  
-# 종목코드 리스트 구하기
-objCpCodeMgr = win32com.client.Dispatch("CpUtil.CpCodeMgr")
-codeList = objCpCodeMgr.GetStockListByMarket(1) #거래소
-codeList2 = objCpCodeMgr.GetStockListByMarket(2) #코스닥
+# 현재가 객체 구하기
+objStockMst = win32com.client.Dispatch("DsCbo1.StockMst")
+objStockMst.SetInputValue(0, 'A005930')   #종목 코드 - 삼성전자
+objStockMst.BlockRequest()
  
+# 현재가 통신 및 통신 에러 처리 
+rqStatus = objStockMst.GetDibStatus()
+rqRet = objStockMst.GetDibMsg1()
+print("통신상태", rqStatus, rqRet)
+if rqStatus != 0:
+    exit()
  
-print("거래소 종목코드", len(codeList))
-for i, code in enumerate(codeList):
-    secondCode = objCpCodeMgr.GetStockSectionKind(code)
-    name = objCpCodeMgr.CodeToName(code)
-    stdPrice = objCpCodeMgr.GetStockStdPrice(code)
-    print(i, code, secondCode, stdPrice, name)
+# 현재가 정보 조회
+code = objStockMst.GetHeaderValue(0)  #종목코드
+name= objStockMst.GetHeaderValue(1)  # 종목명
+time= objStockMst.GetHeaderValue(4)  # 시간
+cprice= objStockMst.GetHeaderValue(11) # 종가
+diff= objStockMst.GetHeaderValue(12)  # 대비
+open= objStockMst.GetHeaderValue(13)  # 시가
+high= objStockMst.GetHeaderValue(14)  # 고가
+low= objStockMst.GetHeaderValue(15)   # 저가
+offer = objStockMst.GetHeaderValue(16)  #매도호가
+bid = objStockMst.GetHeaderValue(17)   #매수호가
+vol= objStockMst.GetHeaderValue(18)   #거래량
+vol_value= objStockMst.GetHeaderValue(19)  #거래대금
  
-print("코스닥 종목코드", len(codeList2))
-for i, code in enumerate(codeList2):
-    secondCode = objCpCodeMgr.GetStockSectionKind(code)
-    name = objCpCodeMgr.CodeToName(code)
-    stdPrice = objCpCodeMgr.GetStockStdPrice(code)
-    print(i, code, secondCode, stdPrice, name)
+# 예상 체결관련 정보
+exFlag = objStockMst.GetHeaderValue(58) #예상체결가 구분 플래그
+exPrice = objStockMst.GetHeaderValue(55) #예상체결가
+exDiff = objStockMst.GetHeaderValue(56) #예상체결가 전일대비
+exVol = objStockMst.GetHeaderValue(57) #예상체결수량
  
-print("거래소 + 코스닥 종목코드 ",len(codeList) + len(codeList2))
+text = '삼성전가 현재가: '+str(offer)
+
+if (exFlag == ord('0')):
+    print("장 구분값: 동시호가와 장중 이외의 시간")
+elif (exFlag == ord('1')) :
+    print("장 구분값: 동시호가 시간")
+elif (exFlag == ord('2')):
+    print("장 구분값: 장중 또는 장종료")
+    
+print("예상체결가 대비 수량")
+print("예상체결가", exPrice)
+print("예상체결가 대비", exDiff)
+print("예상체결수량", exVol)
+
+
+ 
+def post_message(token, channel, text):
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text}
+    )
+    print(response)
+ 
+myToken = "xoxb-3193001508902-3193006392630-wq3LOduRZ1JqpgbFklWsabUI"
+ 
+post_message(myToken,"#stock",text)
+
+os.system("pause")
