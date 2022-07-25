f598727c5eb95f77ac8e22a213102cd5280ce7a6
shkim787 <shkim061198@gmail.com>
2022-07-26 00:20:03 +0900

    넷플릭스 ui 만들기 1

--- 개인공부/Flutter/lib/main.dart
+++ 개인공부/Flutter/lib/main.dart
@@ -1,36 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ex/screen/home_screen.dart';
+import 'package:flutter_ex/widget/bottom_bar.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class _MyAppState extends State<MyApp>{
+  //TabController controller;
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('앱임'),
+       
+      title: 'NetFilx',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        accentColor: Colors.white,
+      ),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body:TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              HomeScreen(),
+              Container(child: Center(child: Text('Search'),),),
+              Container(child: Center(child: Text('Save'),),),
+              Container(child: Center(child: Text('More'),),),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
         ),
-        body: Text('안녕'),
-        bottomNavigationBar: BottomAppBar(
-          child: SizedBox(
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Icon(Icons.phone),
-                Icon(Icons.message),
-                Icon(Icons.contact_page),
-              ],
-            ),
-          )
-        ),
-      )
+      ),
     );
-
   }
 }
